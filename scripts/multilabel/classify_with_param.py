39531774077af7322d358866c98165b8232da591
mmusasabi <kawamata@basicinc.jp>
2018-06-05 16:01:57 +0900

    get tag data

--- scripts/multilabel/classify_with_param.py
+++ scripts/multilabel/classify_with_param.py
@@ -2,6 +2,7 @@ import MeCab
 import sys
 import os
 import pdb # pdb.set_trace() でpry的なことができる。
+import pandas as pd
 
 from sklearn.externals import joblib
 
@@ -26,12 +27,25 @@ def main():
     print('第１引数：' + args[1])
     data = [args[1]]
 
+    # delimiter = "\n"
+    # with open('./train_data/hoge.txt', "rU") as f:
+    #     data = [v.rstrip() for v in f.readlines()]
+
+    # タグのリストを取得
+    data_df = pd.read_csv("./train_data/train_data.tsv", delimiter='\t')
+    genres = list(data_df.drop(['id', 'title', 'content'], axis=1).columns.values)
+
     # 出力
     print("Applying best classifier on test data:")
     predictions = clf.best_estimator_.predict(data)
 
+    tag_list = []
     for predict in predictions :
-        print(predict)
+        for index, value in enumerate(predict):
+            if value == 1 :
+                tag_list.append(genres[index])
+
+    print(tag_list)
 
 if __name__ == "__main__":
     main()
