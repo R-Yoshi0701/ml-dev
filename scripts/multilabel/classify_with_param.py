ff7c52ce052bbbac93bae0746c1b88512c0c69b9
mmusasabi <kawamata@basicinc.jp>
2018-06-05 16:51:29 +0900

    タグの名前配列が返却されるように修正

--- scripts/multilabel/classify_with_param.py
+++ scripts/multilabel/classify_with_param.py
@@ -19,33 +19,33 @@ def tokenize(text):
     return tokens
 
 def main():
+    abs_path = os.path.dirname(os.path.abspath(__file__))
+
     # 学習モデルロード
-    clf = joblib.load(os.path.dirname(os.path.abspath(__file__)) + '/learn_data/clf.pkl')
+    clf = joblib.load(abs_path + '/learn_data/clf.pkl')
 
     # 分類したいデータのロード
     args = sys.argv
-    print('第１引数：' + args[1])
     data = [args[1]]
 
     # delimiter = "\n"
-    # with open('./train_data/hoge.txt', "rU") as f:
+    # with open(abs_path + '/train_data/hoge.txt', "rU") as f:
     #     data = [v.rstrip() for v in f.readlines()]
 
     # タグのリストを取得
-    data_df = pd.read_csv("./train_data/train_data.tsv", delimiter='\t')
+    data_df = pd.read_csv(abs_path + "/train_data/train_data.tsv", delimiter='\t')
     genres = list(data_df.drop(['id', 'title', 'content'], axis=1).columns.values)
 
     # 出力
-    print("Applying best classifier on test data:")
     predictions = clf.best_estimator_.predict(data)
-
     tag_list = []
     for predict in predictions :
         for index, value in enumerate(predict):
             if value == 1 :
                 tag_list.append(genres[index])
 
-    print(tag_list)
+    # result
+    print(','.join(tag_list))
 
 if __name__ == "__main__":
     main()
